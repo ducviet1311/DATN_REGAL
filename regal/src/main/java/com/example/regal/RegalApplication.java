879c44236642688424b3e902cd51f854d25190db
viet <buiducviet101@gmail.com>
2025-02-13 22:47:38 +0700

    init project

---
+++ regal/src/main/java/com/example/regal/RegalApplication.java
@@ -0,0 +1,13 @@
+package com.example.regal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegalApplication.class, args);
+    }
+
+}
