836519892dc1318d8b88bd45c5e8cf152412d81d
dqcuong129 <dqc12924@gmail.com>
2025-04-15 15:03:13 +0700

    fix giao đien thong thin tai khoản và sua loi

--- frontend-user/src/page/thongtintaikhoan.js
+++ frontend-user/src/page/thongtintaikhoan.js
@@ -40,84 +40,84 @@ function ThongTinTaiKhoan() {
   }
 
   return (
-    <>
-      <div class="headeraccount">
-        <h4>Thông tin tài khoản của bạn</h4>
-      </div>
-      <div class="col-lg-6 col-md-8 col-sm-12 col-12 passacc">
-        <form onSubmit={handleChangeInfor} autocomplete="off">
-          <table className="table ">
-            <tr>
-              <th>Họ và tên</th>
-              <td>
-                <input
-                  name="hoten"
-                  defaultValue={user?.hoVaTen}
-                  className="form-control"
-                />
-              </td>
-            </tr>
-            <tr>
-              <th>Ngày sinh</th>
-              <td>
-                <br />
-                <input
-                  name="ngsinh"
-                  defaultValue={user?.ngaySinh}
-                  className="form-control"
-                  type="date"
-                />
-              </td>
-            </tr>
-            <tr>
-              <th>giới tính</th>
-              <td>
-                <br />
-                <div className="d-flex">
-                  <label class="radiocustom">
-                    {" "}
-                    Nam
-                    <input
-                      type="radio"
-                      name="gioitinh"
-                      defaultChecked={true}
-                      value={true}
-                    />
-                    <span class="checkmark"></span>
-                  </label>
-                  <label class="radiocustom" style={{ marginLeft: "20px" }}>
-                    {" "}
-                    Nữ
-                    <input type="radio" name="gioitinh" value={false} />
-                    <span class="checkmark"></span>
-                  </label>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <th>Số điện thoại</th>
-              <td>
-                <br />
-                <input
-                  name="sdt"
-                  defaultValue={user?.soDienThoai}
-                  className="form-control"
-                />
-              </td>
-            </tr>
-            <tr>
-              <td></td>
-              <td>
-                <br />
-                <button className="btn btn-primary form-control">
-                  Cập nhật thông tin
-                </button>
-              </td>
-            </tr>
-          </table>
-        </form>
-      </div>
-    </>
+      <>
+        <div class="headeraccount">
+          <h4>Thông tin tài khoản của bạn</h4>
+        </div>
+        <div class="col-lg-6 col-md-8 col-sm-12 col-12 passacc">
+          <form onSubmit={handleChangeInfor} autocomplete="off">
+            <table className="table ">
+              <tr>
+                <th>Họ và tên</th>
+                <td>
+                  <input
+                      name="hoten"
+                      defaultValue={user?.hoVaTen}
+                      className="form-control"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th>Ngày sinh</th>
+                <td>
+                  <br />
+                  <input
+                      name="ngsinh"
+                      defaultValue={user?.ngaySinh}
+                      className="form-control"
+                      type="date"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th>giới tính</th>
+                <td>
+                  <br />
+                  <div className="d-flex">
+                    <label class="radiocustom">
+                      {" "}
+                      Nam
+                      <input
+                          type="radio"
+                          name="gioitinh"
+                          defaultChecked={true}
+                          value={true}
+                      />
+                      <span class="checkmark"></span>
+                    </label>
+                    <label class="radiocustom" style={{ marginLeft: "20px" }}>
+                      {" "}
+                      Nữ
+                      <input type="radio" name="gioitinh" value={false} />
+                      <span class="checkmark"></span>
+                    </label>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <th>Số điện thoại</th>
+                <td>
+                  <br />
+                  <input
+                      name="sdt"
+                      defaultValue={user?.soDienThoai}
+                      className="form-control"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>
+                  <br />
+                  <button className="btn btn-primary form-control">
+                    Cập nhật thông tin
+                  </button>
+                </td>
+              </tr>
+            </table>
+          </form>
+        </div>
+      </>
   );
 }
 export default ThongTinTaiKhoan;
