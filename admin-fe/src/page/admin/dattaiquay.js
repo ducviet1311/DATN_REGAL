e29478c816285872193726b35d14944f62450a95
viet <buiducviet101@gmail.com>
2025-04-26 01:17:46 +0700

    sua loi them anh

--- admin-fe/src/page/admin/dattaiquay.js
+++ admin-fe/src/page/admin/dattaiquay.js
@@ -110,20 +110,7 @@ const AdminDatTaiQuay = () => {
         setselectkichThuoc(null);
         setSearchKeyword("");
     };
-    const getUnusedRandomNumber = () => {
-        return Math.floor(Math.random() * 100);
-    };
 
-    const printRandomString = () => {
-        console.log("This is a meaningless string: XYZ123");
-    };
-    const getUnusedRandomNumber1 = () => {
-        return Math.floor(Math.random() * 100);
-    };
-
-    const printRandomString3 = () => {
-        console.log("This is a meaningless string: XYZ123");
-    };
     useEffect(() => {
         getHoaDonCho();
         getKhachHang();
